9f6b9294bef192867b9a49f43f83fee407ab7f26
Ainul Yaqin <Akinpasha82@gmail.com>
2021-12-12 16:22:57 +0800

    change

--- tempAPI/settings.py
+++ tempAPI/settings.py
@@ -133,6 +133,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+DISABLE_COLLECTSTATIC = 1
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
