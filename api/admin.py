5fba56ef38343283d76766a519fdcfa9b0e402ad
Ainul Yaqin <Akinpasha82@gmail.com>
2021-12-17 10:48:26 +0800

    update

--- api/admin.py
+++ api/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import tempData
 
 class tempField(admin.ModelAdmin):
-    list_display = ['temp', 'img', 'created_at']
+    list_display = ['temp_obj', 'temp_env', 'img', 'created_at']
 
 admin.site.register(tempData, tempField)
--- api/models.py
+++ api/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class tempData(models.Model):
-    temp = models.FloatField()
+    temp_obj = models.FloatField(null=True)
+    temp_env = models.FloatField(null=True)
     img = models.ImageField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
